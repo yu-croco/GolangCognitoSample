e9f38f4548564f0827e86274ff9a4373afda49d3
yu-croco <yuki.kita22@gmail.com>
2020-10-31 10:34:25 +0900

    implement logic

--- main.go
+++ main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 )
 
-type Response struct {
-	Message string `json:"message"`
+// see: https://docs.aws.amazon.com/code-samples/latest/catalog/go-cognito-CognitoListUsers.go.html
+func NewCognitoSession()*cognitoidentityprovider.CognitoIdentityProvider {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	return cognitoidentityprovider.New(sess)
+}
+func findSubs(userList *cognitoidentityprovider.ListUsersOutput) []string {
+	var userSubs []string
+
+	for _, user := range userList.Users {
+		for _, attr := range user.Attributes {
+			if *attr.Name == "sub" {
+				userSubs = append(userSubs, *attr.Value)
+			}
+		}
+	}
+	return userSubs
 }
 
-func Handler() (Response, error) {
-	return Response{
-		Message: "Go Serverless v1.0! Your function executed successfully!",
-	}, nil
+func Handler() error {
+	userPoolId := "ADD ME!!"
+
+	userList, cognitoErr := NewCognitoSession().
+		ListUsers(&cognitoidentityprovider.ListUsersInput{UserPoolId: &userPoolId})
+	if cognitoErr != nil {
+		return cognitoErr
+	}
+
+	userSubs := findSubs(userList)
+
+	fmt.Println("userSubs: ", userSubs)
+
+	return nil
 }
 
 func main() {
